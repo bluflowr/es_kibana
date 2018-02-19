fdf07c01cff2243859a5407b035df111560fa285
Irma Kramer <bluflowr@gmail.com>
2018-02-19 14:00:54 -0600

    Add print to basic script

--- es_python.py
+++ es_python.py
@@ -1,9 +1,13 @@
-from datetime import datetime
 from elasticsearch import Elasticsearch
 es = Elasticsearch() #defaults to localhost:9200
 
-# ignore 400 cause by IndexAlreadyExistsException when creating an index
-es.indices.create(index='test-index', ignore=400)
+# Basics for quickly creating and deleting an elasticsearch index
 
+# ignore 400 cause by IndexAlreadyExistsException when creating an index
+print('Creating Elastic Search Index')
+response = es.indices.create(index='test-index', ignore=400)
+print(response)
 # ignore 404 and 400
-es.indices.delete(index='test-index', ignore=[400, 404])
+print('Deleting Elastic Search Index')
+response = es.indices.delete(index='test-index', ignore=[400, 404])
+print(response)
